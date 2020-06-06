c6b26ec9d673e1f2306eb6afec3f5094cd95c643
andreasjost <codefactory@gmx.ch>
2020-06-06 08:26:42 +0000

    Renamed app: signup -> profiles

--- eggbox/settings.py
+++ eggbox/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'landing',
     'planning',
-    'signup',
+    'profiles',
 ]
 
 MIDDLEWARE = [
--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup')
+    path('', views.profiles, name='profiles')
 ]
