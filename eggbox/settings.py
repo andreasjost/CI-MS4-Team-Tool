f41cf0826110f20744c2fd812a0b961250d641a5
andreasjost <codefactory@gmx.ch>
2020-06-10 13:45:19 +0000

    Edited Allauth base-template + styles

--- eggbox/settings.py
+++ eggbox/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
     'crispy_forms',
     'landing',
     'planning',
--- profiles/models.py
+++ profiles/models.py
@@ -32,7 +32,7 @@ class UserProfile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+def update_user_profile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
