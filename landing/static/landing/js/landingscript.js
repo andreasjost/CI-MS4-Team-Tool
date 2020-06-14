4383be3ef84c5329eab63d7943013fbac4cfaa94
andreasjost <codefactory@gmx.ch>
2020-06-14 21:06:40 +0000

    Edited design of the landing page (logo and tagline)

---
+++ landing/static/landing/js/landingscript.js
@@ -0,0 +1,10 @@
+console.log("here")
+
+$(document).ready(function(){
+    $(document).scroll(function() {
+        var alpha = Math.min(0.5 + 0.4 * $(this).scrollTop() / 210, 0.9);
+        var channel = Math.round(alpha * 255);
+        $("body").css('background-color', 'rgb(' + channel + ',' + channel + ',' + channel + ')');
+    });
+    console.log("here")
+});
