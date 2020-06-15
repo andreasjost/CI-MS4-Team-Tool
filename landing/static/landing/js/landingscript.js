cc994e4f311df8f14f255c5278862207d813b44e
andreasjost <codefactory@gmx.ch>
2020-06-15 17:35:12 +0000

    Changed style of the landing page

--- landing/static/landing/js/landingscript.js
+++ landing/static/landing/js/landingscript.js
@@ -2,9 +2,14 @@ console.log("here")
 
 $(document).ready(function(){
     $(document).scroll(function() {
-        var alpha = Math.min(0.5 + 0.4 * $(this).scrollTop() / 210, 0.9);
-        var channel = Math.round(alpha * 255);
-        $("body").css('background-color', 'rgb(' + channel + ',' + channel + ',' + channel + ')');
+        let top = $(document).scrollTop();
+        el = document.getElementById("hero-image");
+        
+        if (top < 280) {
+            let newHight = 280 - top;
+            if (newHight > 55) {
+            el.style.height = newHight + 'px';
+            }
+        }
     });
-    console.log("here")
 });
