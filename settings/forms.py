b42700f7afbe6f4aa934b177bd70f8ff8d4d074a
andreasjost <codefactory@gmx.ch>
2020-06-17 15:45:09 +0000

    Added content to global settings template, added corresponding form to app

--- settings/forms.py
+++ settings/forms.py
@@ -1,5 +1,32 @@
-"""
-for field in self.fields:
+from django import forms
+from profiles.models import CompanyProfile
+
+
+class CompanyProfileForm(forms.ModelForm):
+    class Meta:
+        model = CompanyProfile
+        exclude = ('company_id',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'company_name': 'Company name',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'country': 'Country or State',
+            'postcode': 'Postcode',
+            'town_or_city': 'Town or City',
+            'plan': 'Plan',
+            'signup_date': 'Date Signed up',
+            'renewal_date': 'Last Plan renewal',
+            'payment': 'Paid for number of months',
+            'setting_weekstart': 'The Day your week starts'
+        }
+
+        for field in self.fields:
             if field != 'country':
                 if self.fields[field].required:
                     placeholder = f'{placeholders[field]} *'
@@ -7,5 +34,4 @@ for field in self.fields:
                     placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
-            self.fields[field].label = False
-"""
+            self.fields[field].label = placeholder
--- settings/views.py
+++ settings/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 
-from profiles.models import CompanyProfile
-from profiles.models import UserProfile
+from profiles.models import UserProfile, CompanyProfile
+from .forms import CompanyProfileForm
 
 
 def settings_global(request):
     """ Show the global settings According to the company id
     """
     profile = get_object_or_404(UserProfile, user=request.user)
-    print(profile.company_id)
     company = get_object_or_404(CompanyProfile, company_id=profile.company_id)
 
+    if request.method == 'POST':
+        form = CompanyProfileForm(request.POST, instance=company)
+        if form.is_valid():
+            form.save()
+            # messages.success(request, 'Profile updated successfully')
+        else:
+            print("unsuccessful")
+            # messages.error(request, 'Update failed. Please ensure the form is valid.')
+
+    else:
+        form = CompanyProfileForm(instance=company)
+
     context = {
+        'profile': profile,
         'company': company,
+        'form': form
     }
 
     return render(request, 'settings/settings_global.html', context)
