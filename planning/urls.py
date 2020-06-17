3576f3154547f7f7f91c22ef2e9dbe65cce0c918
andreasjost <codefactory@gmx.ch>
2020-06-17 09:15:20 +0000

    Moved function month-plus/minus from frontend(JS) to backend

--- planning/urls.py
+++ planning/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.planning, name='planning'),
-    path('summary/<int:user_id>/', views.summary, name='summary')
+    path('summary/<int:user_id>/', views.summary, name='summary'),
+    path('month_plus/', views.month_plus, name='month_plus'),
+    path('month_minus/', views.month_minus, name='month_minus')
 ]
--- planning/views.py
+++ planning/views.py
@@ -2,21 +2,80 @@ from django.shortcuts import render, get_object_or_404
 from profiles.models import UserProfile
 from .models import Event, EventCategory
 from django.core import serializers  # used to use template variables in JS
+import datetime
+
 
 
 def planning(request):
     """
     Get the logged in user profile and show the planning template
+    and submit the current month+year
     """
+    # set the session variable for month and year
+    if 'sel_month' not in request.session:
+        now = datetime.datetime.now()
+        request.session['sel_month'] = now.month-1
+        request.session['sel_year'] = now.year
     profile = get_object_or_404(UserProfile, user=request.user)
-    # users = UserProfile.objects.all()
-
     users = serializers.serialize("json", UserProfile.objects.all())
+
+    now_json = '{"month": "%s", "year": "%s"}' % (request.session['sel_month'], request.session['sel_year'])
 
     template = 'planning/planning.html'
     context = {
         'profile': profile,
-        'users': users
+        'users': users,
+        'mmyyyy': now_json
+    }
+
+    return render(request, template, context)
+
+
+def month_plus(request):
+    """ user pushed one month ahead in the planning """
+    month = request.session['sel_month']
+    year = request.session['sel_year']
+    month += 1
+    if month == 12:
+        month = 0
+        year += 1
+    request.session['sel_month'] = month
+    request.session['sel_year'] = year
+    profile = get_object_or_404(UserProfile, user=request.user)
+    users = serializers.serialize("json", UserProfile.objects.all())
+
+    now_json = '{"month": "%s", "year": "%s"}' % (request.session['sel_month'], request.session['sel_year'])
+
+    template = 'planning/planning.html'
+    context = {
+        'profile': profile,
+        'users': users,
+        'mmyyyy': now_json
+    }
+
+    return render(request, template, context)
+
+
+def month_minus(request):
+    """ user pushed one month back in the planning """
+    month = request.session['sel_month']
+    year = request.session['sel_year']
+    month -= 1
+    if month == -1:
+        month = 11
+        year -= 1
+    request.session['sel_month'] = month
+    request.session['sel_year'] = year
+    profile = get_object_or_404(UserProfile, user=request.user)
+    users = serializers.serialize("json", UserProfile.objects.all())
+
+    now_json = '{"month": "%s", "year": "%s"}' % (request.session['sel_month'], request.session['sel_year'])
+
+    template = 'planning/planning.html'
+    context = {
+        'profile': profile,
+        'users': users,
+        'mmyyyy': now_json
     }
 
     return render(request, template, context)
