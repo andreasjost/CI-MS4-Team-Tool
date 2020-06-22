1cbb77e09700d0c7989dc8d29872d87d66e52505
andreasjost <codefactory@gmx.ch>
2020-06-22 14:09:14 +0000

    Adjusted month navigation in the planning app, now year works

--- planning/urls.py
+++ planning/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.planning, name='planning'),
     path('summary/<int:user_id>/', views.summary, name='summary'),
-    path('month_change/<int:new_month>/', views.month_change, name='month_change'),
-    path('month_current/', views.month_current, name='month_current'),
 ]
--- planning/views.py
+++ planning/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from profiles.models import UserProfile
 from settings.models import Team
 from .models import Event
 from django.core import serializers  # used to use template variables in JS
-import datetime
+from datetime import datetime, timedelta
 
 
 def planning(request):
@@ -11,52 +11,77 @@ def planning(request):
     Get the logged in user profile and show the planning template
     and submit the current month+year
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    teams = Team.objects.filter(company_id=profile.company_id)
     # set the session variable for month and year
     if 'sel_month' not in request.session:
-        now = datetime.datetime.now()
-        request.session['sel_month'] = now.month-1
-        request.session['sel_year'] = now.year
+        date_now = datetime.now()
+        request.session['sel_month'] = date_now.month
+        request.session['sel_year'] = date_now.year
+
+    sel_month = int(request.session['sel_month'])
+    sel_year = int(request.session['sel_year'])
+    date_sel = datetime(sel_year, sel_month, 1)
+    previous_month = date_sel + timedelta(days=-10)
+    next_month = date_sel + timedelta(days=40)
+
+    navmonth = {
+        'previous': previous_month.month,
+        'next': next_month.month,
+        'current': request.session['sel_month']
+    }
+
+    if 'month' in request.GET:
+        month = int(request.GET['month'])
+        if month == 0:
+            date_now = datetime.now()
+            request.session['sel_month'] = date_now.month
+            request.session['sel_year'] = date_now.year
+
+            return redirect(reverse('planning'))
+
+        else:
+            year = int(request.session['sel_year'])
+            if date_sel.month == 12 and month == 1:
+                year += 1
+
+            if date_sel.month == 1 and month == 12:
+                year -= 1
+
+            date_sel = date_sel.replace(month=month)
+            previous_month = date_sel + timedelta(days=-10)
+            next_month = date_sel + timedelta(days=40)
+
+            request.session['sel_month'] = month
+            request.session['sel_year'] = year
+
+            return redirect(reverse('planning'))
+
+    team = ''
+
+    if 'team' in request.GET:
+        team = request.GET['team']
+
+    elif profile.level == 'agent' or profile.level == 'manager':
+        team = profile.team
+
+    else:
+        team = teams[0]
+
+    js_month = int(request.session['sel_month'])-1
+
+    users = serializers.serialize("json", UserProfile.objects.filter(company_id=profile.company_id, team__team_name__icontains=team))
+    now_json = '{"month": "%s", "year": "%s"}' % (js_month, request.session['sel_year'])
 
     template = 'planning/planning.html'
-    context = render_data(request)
-
-    return render(request, template, context)
-    
-
-
-def month_change(request, new_month):
-    """
-    user pushed one month forward or back
-    
-    --- YEAR BREAK DOESN'T WORK. FIX IT ---
-    
-    """
-    # month = request.session['sel_month']
-    year = request.session['sel_year']
-    if new_month > 12:
-        new_month = 0
-        year += 1
-    if new_month < 0:
-        new_month = 11
-        year -= 1
-    request.session['sel_month'] = new_month
-    request.session['sel_year'] = year
-
-    template = 'planning/planning.html'
-    context = render_data(request)
-
-    return render(request, template, context)
-
-
-def month_current(request):
-    """
-    Reset the month
-    """
-    now = datetime.datetime.now()
-    request.session['sel_month'] = now.month-1
-    request.session['sel_year'] = now.year
-    template = 'planning/planning.html'
-    context = render_data(request)
+    context = {
+        'profile': profile,
+        'users': users,
+        'teams': teams,
+        'current_team': team,
+        'mmyyyy': now_json,
+        'nav_month': navmonth
+    }
 
     return render(request, template, context)
 
@@ -73,42 +98,3 @@ def summary(request, user_id):
     }
 
     return render(request, template, context)
-
-
-def render_data(request):
-    profile = get_object_or_404(UserProfile, user=request.user)
-    teams = Team.objects.filter(company_id=profile.company_id)
-
-    team = ''
-
-    if 'team' in request.GET:
-        team = request.GET['team']
-
-    elif profile.level == 'agent' or profile.level == 'manager':
-        team = profile.team
-
-    else:
-        team = teams[0]
-
-    # users = serializers.serialize("json", UserProfile.objects.filter(company_id=profile.company_id))
-    # users = serializers.serialize("json", UserProfile.objects.filter(company_id=profile.company_id, team__team_name__in=team))
-    users = serializers.serialize("json", UserProfile.objects.filter(company_id=profile.company_id, team__team_name__icontains=team))
-
-
-    now_json = '{"month": "%s", "year": "%s"}' % (request.session['sel_month'], request.session['sel_year'])
-    navmonth = {
-        'previous': request.session['sel_month'] - 1,
-        'next': request.session['sel_month'] + 1,
-        'current': request.session['sel_month']
-    }
-
-    context = {
-        'profile': profile,
-        'users': users,
-        'teams': teams,
-        'current_team': team,
-        'mmyyyy': now_json,
-        'nav_month': navmonth
-    }
-
-    return context
