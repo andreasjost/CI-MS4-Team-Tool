99c6c93664b514ad91d9cc0797a258396061df94
andreasjost <codefactory@gmx.ch>
2020-06-27 21:41:27 +0000

    Added content to planning template (welcome.html include)

--- coaching/views.py
+++ coaching/views.py
@@ -9,7 +9,11 @@ from .models import Coaching
 @login_required
 def coaching(request):
     profile = get_object_or_404(UserProfile, user=request.user)
-    coachings = Coaching.objects.filter(user_id=profile.user_id)
+    if profile.level == 'manager':
+        coachings = Coaching.objects.filter(user_id=profile.user_id)
+    
+    elif profile.level == 'agent':
+        coachings = Coaching.objects.filter(user_id=profile.user_id)
 
     template = 'coaching/coaching.html'
     context = {
